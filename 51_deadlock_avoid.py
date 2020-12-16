a2a1f3573dcee7f46891e03b379bdabef15adcfb
damminhtien <damminhtienchl@gmail.com>
2020-12-16 16:29:49 +0700

    update readme.md content: p0,p1

--- 51_deadlock_avoid.py
+++ 51_deadlock_avoid.py
@@ -1,3 +1,14 @@
+'''In multithreaded programs, a common source of deadlock is due to threads that attempt
+to acquire multiple locks at once. For instance, if a thread acquires the first lock, but
+then blocks trying to acquire the second lock, that thread can potentially block the
+progress of other threads and make the program freeze.
+One solution to deadlock avoidance is to assign each lock in the program a unique
+number, and to enforce an ordering rule that only allows multiple locks to be acquired
+in ascending order.
+The key to this recipe lies in the first statement that sorts the locks according to object
+identifier. By sorting the locks, they always get acquired in a consistent order regardless
+of how the user might have provided them to acquire().
+'''
 import threading
 from contextlib import contextmanager
 
